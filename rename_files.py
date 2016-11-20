db828a1c493b38f269f0fc377afe6778eecfa60c
Weili Yi <yi@weiliy.net>
2016-11-21 01:35:27 +0800

    feat: rename function

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,14 @@
 import os
-def renames_files():
+
+def rename_files():
     #(1) get file names form a folder
+    file_list = os.listdir(r"./prank/")
 
     #(2) for each file, rename filename
+    saved_path = os.getcwd()
+    os.chdir(r"./prank/")
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
 
 rename_files()
